30af6fe6f45ca97d59e1b732b54f466796d4aea8
Salo Charabati <salochara@gmail.com>
2018-11-19 10:58:38 +0200

    Added devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #  Email sender for the signup
+  config.mailer_sender = 'no-reply@at-home.com'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # (might change this for Heoroku)
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
