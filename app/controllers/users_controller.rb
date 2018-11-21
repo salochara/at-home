7dceb47b13c4b7d10956d12c2f0b3f8561c4f27b
Salo Charabati <salochara@gmail.com>
2018-11-21 11:32:48 +0200

    Profile picture upload working and picutre is showing it in the navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :photo)
   end
 
 end
