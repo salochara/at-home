a2ee3a832d76837b31936ac559af8dd63b71a8b3
Salo Charabati <salochara@gmail.com>
2018-11-21 15:44:55 +0200

    gc

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -9,4 +9,27 @@ if (mapElement) {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10'
   });
+
+  const markers = JSON.parse(mapElement.dataset.markers);
+
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
+  })
+
+  if (markers.length === 0) {
+    map.setZoom(1);
+  } else if (markers.length === 1) {
+    map.setZoom(14);
+    map.setCenter([markers[0].lng, markers[0].lat]);
+  } else {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    map.fitBounds(bounds, { duration: 0, padding: 75 })
+  }
 }
+
+
